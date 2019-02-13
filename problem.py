2568259b9b2eddb63c412867e0e4326647c87202
maikia <maria@telenczuk.pl>
2019-02-13 13:36:03 +0100

    converted mask to list of sparse matrices

--- problem.py
+++ problem.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import rampwf as rw
+import scipy.sparse as sps
 from nilearn.image import load_img
 from sklearn.model_selection import StratifiedShuffleSplit
 
@@ -41,11 +42,11 @@ def _read_ids(path, split='train'):
     train_id = rng.choice(ids, size=int(ids.size * 0.8), replace=False)
     test_id = np.setdiff1d(ids, train_id, assume_unique=True)
 
-    # FIXME: return only 20 first IDs 
+    # FIXME: return only 7 first IDs 
     if split == 'train':
-        return train_id[:20]
+        return train_id[:7]
 
-    return test_id[:20]
+    return test_id[:7]
 
 def _get_patient_path(path, subject_id):
     path_metadata = os.path.join(
@@ -54,44 +55,45 @@ def _get_patient_path(path, subject_id):
     df = pd.read_csv(path_metadata)
     # FIXME: just to take the first occurrence
     site_dir = df[df['INDI Subject ID'] == subject_id]['INDI Site ID'].iloc[0]
-    subject_id = '{:06d}'.format(subject_id)
+    subject_id = _get_str_subject_id(subject_id)
     path_patient = os.path.join(
         path, 'data', 'images', site_dir, subject_id, 't01'
     )
     return path_patient
 
+def _get_str_subject_id(subject_id):
+    return '{:06d}'.format(subject_id)
+
 def _read_brain_image(path, subject_id):
-    path_metadata = os.path.join(
-        path, 'data', 'images', 'ATLAS_Meta-Data_Release_1.1_standard_mni.csv'
-    )
-    df = pd.read_csv(path_metadata)
-    # FIXME: just to take the first occurrence
-    site_dir = df[df['INDI Subject ID'] == subject_id]['INDI Site ID'].iloc[0]
-    subject_id = '{:06d}'.format(subject_id)
-    
+    # the data will be of dimensions: #img,h,v,d
     path_patient = _get_patient_path(path, subject_id)
     path_brain_image = os.path.join(path_patient,
-        subject_id + '_t1w_deface_stx.nii.gz'
+        _get_str_subject_id(subject_id) + '_t1w_deface_stx.nii.gz'
     )
     return load_img(path_brain_image).get_data()
 
 def _combine_masks(path_masks):
     mask = load_img(path_masks[0]).get_data()
     for next_mask_path in path_masks[1:]:
-        np.add(mask, load_img(next_mask_path).get_data())
+        mask = np.add(mask, load_img(next_mask_path).get_data())
 
-    mask[mask > 1] = 1
+    # masks are to be only 0s for no lesion or 1s for lesion
+    mask[mask > 1] = 1 
     return mask
 
 def _read_stroke_segmentation(path, subject_id):
+    # the data will be of dimensions: #img,h,sparse(v,d)
     path_patient = _get_patient_path(path, subject_id)
     path_masks = glob.glob(str(path_patient)+'/*_LesionSmooth_*.nii.gz')
     
     mask = _combine_masks(path_masks)  
+
+    # convert 3D numpy array to list of 2D sparse matrices
+    mask = [sps.csr_matrix(mask[idx]) for idx in range(np.size(mask,0))]
     return mask  
 
 def _read_data(path, split_ids):
-    # the data will be of dimensions: #img,h,v,d
+    
     X = np.stack([_read_brain_image(path, subject_id)
                    for subject_id in split_ids])
 
