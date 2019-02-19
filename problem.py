2379e64e4c04a6f6182ffc59e80be9d9f1212c37
maikia <maria@telenczuk.pl>
2019-02-19 10:13:03 +0100

    finished load() in the ImageLoader in the problem.py for now

--- problem.py
+++ problem.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 import submissions.starting_kit.classifier as classifier 
-#from ..utils.importing import import_file
+
 
 import glob
 import os
@@ -24,11 +24,11 @@ workflow = rw.workflows.Classifier()
 
 score_types = [
     rw.score_types.Accuracy(name='acc'),
-    #rw.score_types.ClassificationError(name='error'),
-    #rw.score_types.NegativeLogLikelihood(name='nll'),
-    #rw.score_types.F1Above(name='f1_70', threshold=0.7),
+    #rw.score_types.DiceCoefficient(name='dice'),
 ]
 
+#####################################################
+# FIXME: this eventually should be added to rw.score_types....
 def dice_coefficient(y_pred, y_true):
     # Dice’s coefficient (DC), which describes the volume overlap between two segmentations 
     # and is sensitive to the lesion size;
@@ -38,6 +38,30 @@ def dice_coefficient(y_pred, y_true):
         dice = (np.sum((y_pred==1)&(y_true==1))*2.0) / (np.sum(y_pred) + np.sum(y_true))
         #print('Dice coefficient is {}'.format(dice))
         return dice
+
+def average_symmetric_surface_distance(y_pred, y_true):
+    # ASSD: denotes the average surface distance between two segmentations
+    # 1. define the average surface distance (ASD)
+    # - get all the surface voxels
+    # - 
+    
+    # 2. average over both directions
+    
+    
+    
+    pass
+    
+def hausdorff_distance(y_pred, y_true):
+    # a measure of the maximum surface distance, hence especially sensitive to outliers
+    # maximum of all surface distances
+    
+    
+    pass
+    
+    
+    
+    
+########################################################
 
 # cross validation
 def get_cv(X, y):
@@ -140,8 +164,12 @@ def get_test_data(path='.'):
     #return _read_data(path, test_id)
     return test_id
     
-#import submissions.starting_kit.segmentation_classifier as segmentation_classifier
+    
+    
+# FIXME: when added to workflow change those lines:
+#from ..utils.importing import import_file
 import submissions.starting_kit.segmentation_classifier as segmentation_classifier 
+
 class SimplifiedSegmentationClassifier(object):
     """
     SimplifiedSegmentationClassifier workflow.
@@ -165,7 +193,7 @@ class SimplifiedSegmentationClassifier(object):
         self.element_names = workflow_element_names
         self.data_file = data_file
 
-    def train_submission(self, module_path, patient_idxs): 
+    def train_submission(self, module_path, patient_ids): 
         """Train an image classifier.
         module_path : str
             module where the submission is. the folder of the module
@@ -175,20 +203,27 @@ class SimplifiedSegmentationClassifier(object):
              which are to be used for training. 
         """
         
+        # FIXME: when added to workflow add those lines:
         #segmentation_classifier = import_file(module_path, self.element_names[0])
-        #pred_prob = segmentation_classfier.SegmenatationClassifier()
-        #submissions.starting_kit.
         
-        # FIXME: have module_path instead
         clf = segmentation_classifier.SegmentationClassifier()
         
         # load image one by one
-        for patient_id in patient_idxs:
-            X = self._read_brain_image(module_path, patient_id)
-            y_true = self._read_stroke_segmentation(module_path, patient_id)
-            fitit = clf.fit(X, y_true)
-            del X
-            del y_true
+        #for patient_id in patient_idxs:
+        #    X = self._read_brain_image(module_path, patient_id)
+        #    y_true = self._read_stroke_segmentation(module_path, patient_id)
+        #    fitit = clf.fit(X, y_true)
+        #    del X
+        #    del y_true
+        #return clf
+        
+        img_loader = ImageLoader(
+            patient_ids,
+            #X_array[train_is], y_array[train_is],
+            #folder=folder,
+            n_classes=self.n_classes
+        )
+        clf.fit(img_loader)
         return clf
 
     def test_submission(self, module_path, trained_model, patient_idxs):
@@ -217,29 +252,178 @@ class SimplifiedSegmentationClassifier(object):
             del y_pred
         return dices
         
+        
+class ImageLoader(object):
+    """
+    Load and image and optionally its segmented mask.
+    In segmentation_classifier.py, both `fit` and `predict` take as input
+    an instance of `ImageLoader`.
+    ImageLoader is used in `fit` and `predict` to either load one 3d image
+    and its corresponding segmented mask  (at training time), or one 3d image (at test
+    time).
+    Images are loaded by using the method `load`.
+    Parameters
+    ==========
+    X_array : ArrayContainer of int
+        vector of image IDs to train on
+         (it is named X_array to be coherent with the current API,
+         but as said here, it does not represent the data itself,
+         only image IDs) corresponding to images from the given .csv file
+    folder : str
+        folder where the data_file is
+    data_file : str
+        name of the .csv file, with Img id, it's corresponding patient Id (INDI Subject ID)
+        and where this particular data stored
+    n_classes : int
+        Total number of classes.
+    """
+
+    def __init__(self, img_ids, folder='data/images/', run_type = 'train',
+                data_file='ATLAS_Meta-Data_Release_1.1_standard_mni.csv',
+                n_classes=[0,1], 
+                train_suffix='_t1w_deface_stx.nii.gz', test_suffix='_LesionSmooth_*.nii.gz'):
+        self.data_file = data_file
+        self.folder = folder
+        self.img_ids = img_ids
+        self.n_classes = n_classes
+        self.nb_examples = len(img_ids)
+        self.train_suffix = train_suffix
+        self.test_suffix = test_suffix
+        self.run_type = run_type
+
+    def load(self, index):
+        """
+        Load one image and its corresponding segmented mask (at training time),
+        or one image (at test time).
+        Parameters
+        ==========
+        index : int
+            Index of the image to load.
+            It should be between 0 and self.nb_examples - 1
+        Returns
+        =======
+        either a tuple `(x, y)` or `x`, where:
+            - x is a numpy array of shape (height, width, depth),
+              and corresponds to the image of the requested `index`.
+            - y is a numpy array of the same shape as x, however filled only with 
+                integers of n_classes
+        At training time, `y` is given, and `load` returns
+        a tuple (x, y).
+        At test time, `y` is `None`, and `load` returns `x`.
+        """
+        from nilearn.image import load_img
+       
+        subject_id, path_patient = self._get_patient_path(img_id=index, 
+                                            path=self.folder, data_file=self.data_file)
+        
+        x = self._read_brain_image(subject_id=subject_id, path_patient=path_patient)
+        
+        if self.run_type == 'train':
+            y = self._read_stroke_segmentation(subject_id=subject_id, path_patient=path_patient)
+            return x, y
+        elif self.run_type == 'test':
+            return x
+
+    def parallel_load(self, indexes, transforms=None):
+        """
+        Load and image and optionally its mask.
+        Load one image and its corresponding mask (at training time),
+        or one image (at test time).
+        Parameters
+        ==========
+        index : int
+            Index of the image to load.
+            It should in between 0 and self.nb_examples - 1
+        Returns
+        =======
+        either a tuple `(x, y)` or `x`, where:
+            - x is a numpy array of shape (height, width, nb_color_channels),
+              and corresponds to the image of the requested `index`.
+            - y is an integer, corresponding to the class of `x`.
+        At training time, `y_array` is given, and `load` returns
+        a tuple (x, y).
+        At test time, `y_array` is `None`, and `load` returns `x`.
+        """
+        from skimage.io import imread
+        from joblib import delayed, Parallel, cpu_count
+
+        for index in indexes:
+            assert 0 <= index < self.nb_examples
+
+        n_jobs = cpu_count()
+        filenames = [
+            os.path.join(self.folder, '{}'.format(self.X_array[index]))
+            for index in indexes]
+        xs = Parallel(n_jobs=n_jobs, backend='threading')(
+            delayed(imread)(filename) for filename in filenames)
+
+        if transforms is not None:
+            from functools import partial
+            transform = partial(_image_transform, transforms=transforms)
+            xs = Parallel(n_jobs=n_jobs, backend='threading')(
+                delayed(transform)(x) for x in xs)
+
+        if self.y_array is not None:
+            ys = [self.y_array[index] for index in indexes]
+            return xs, ys
+        else:
+            return xs
+
+    def __iter__(self):
+        for i in range(self.nb_examples):
+            yield self.load(i)
+
+    def __len__(self):
+        return self.nb_examples
             ######################read single data image#################################
-    def _read_brain_image(self, path, subject_id):
-        # the data will be of dimensions: #img,h,v,d
-        path_patient = _get_patient_path(path, subject_id)
+    def _get_patient_path(self, img_id, path, data_file):
+        path_metadata = os.path.join(path, data_file)
+        df = pd.read_csv(path_metadata)
+        #site_dir = df[df['Img Id'] == img_id]['INDI Site ID']
+        subject = df[df['Img Id'] == img_id]
+        site_dir = subject['INDI Site ID'].iloc[0]
+        subject_id = subject['INDI Subject ID'].iloc[0]
+        subject_id_str = self._get_str_subject_id(subject_id)
+        session = subject['Session'].iloc[0]
+        session=session.replace(" ", "")
+        
+        path_patient = os.path.join(
+            path, site_dir, subject_id_str, session
+        )
+        return subject_id, path_patient
+        
+    def _get_str_subject_id(self, subject_id):
+        return '{:06d}'.format(subject_id)
+    
+    def _read_brain_image(self, subject_id, path_patient):
+        # the data will be of dimensions: #h,v,d
+
         path_brain_image = os.path.join(path_patient,
-            _get_str_subject_id(subject_id) + '_t1w_deface_stx.nii.gz'
+            self._get_str_subject_id(subject_id) + self.train_suffix
         )
         return load_img(path_brain_image).get_data()
+   
     
     def _combine_masks(self, path_masks):
+        print(path_masks)
         mask = load_img(path_masks[0]).get_data().astype(bool)
         for next_mask_path in path_masks[1:]:
             mask2 = load_img(next_mask_path).get_data().astype(bool)
             mask|=mask2
+            #mask = np.add(mask, load_img(next_mask_path).get_data())
         mask = mask.astype(np.uint8)
-        assert np.all(np.isin(mask,self.n_classes)) # mask must have only values as in the self.n_classes
+        # masks are to be only 0s for no lesion or 1s for lesion
+        #mask[mask > 1] = 1 
         return mask
 
-    def _read_stroke_segmentation(self, path, subject_id):
-        path_patient = _get_patient_path(path, subject_id)
-        path_masks = glob.glob(str(path_patient)+'/*_LesionSmooth_*.nii.gz')
+    def _read_stroke_segmentation(self, subject_id, path_patient):
+        #path_patient = _get_patient_path(path, subject_id)
+        str_subject_id = self._get_str_subject_id(subject_id)
+        print(str(path_patient)+'/'+str_subject_id +self.test_suffix)
         
-        mask = _combine_masks(path_masks)  
+        path_masks = glob.glob(str(path_patient)+'/'+str_subject_id +self.test_suffix) # get all the lesions
+        
+        mask = self._combine_masks(path_masks)  
         assert np.all(np.isin(mask,self.n_classes)) # mask must have only values as in the self.n_classes
         # convert 3D numpy array to list of 2D sparse matrices
         # mask = [sps.csr_matrix(mask[idx]) for idx in range(np.size(mask,0))]
