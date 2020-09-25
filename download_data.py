ca828bcba3c4a53ac06f3c3f6dcd100fa9519f83
maikia <maja_ka@hotmail.com>
2020-09-25 09:15:22 +0200

    improve download from osf

--- download_data.py
+++ download_data.py
@@ -12,7 +12,7 @@ from osfclient.api import OSF
 # this script does the same as (from terminal)
 # osf upload local_path remote_path
 
-LOCAL_PATH = 'data/'  # local path to the data
+LOCAL_PATH = 'data'  # local path to the data
 REMOTE_PATH = 'stroke/'  # remote path where to store the data on OSF
 PROJECT_CODE = 't4uf8'  # to find your PROJECT_CODE navigate to your OSF
 # project on the web. The link will be something of this type:
@@ -32,31 +32,28 @@ def download_from_osf():
     for file_ in store.files:
         # get only those files which are stored in REMOTE_PATH
         pathname = file_.path
+
         if REMOTE_PATH not in pathname:
             # we are not interested in this file
             continue
         # otherwise we are copying it locally
         # check if the directory tree exists and add the dirs if necessary
 
-        dirname, filename = os.path.split(pathname)
+        # do not include project name
+        pathname = pathname[pathname.find(REMOTE_PATH)+len(REMOTE_PATH):]
+        save_file = os.path.join(LOCAL_PATH, pathname)
+        pathfile, filename = os.path.split(save_file)
 
-        dirs = pathname.split('/')[1:-1]  # we are only interested in dirs
-        dirs[0] = LOCAL_PATH  # we are overwriting the project name with local
-        # path
-        # check if all the necessary dirs exist and create them otherwise
-        deeper_path = dirs[0]
-        for idx, check_dir in enumerate(dirs):
-            if idx > 0:
-                deeper_path = os.path.join(deeper_path, check_dir)
-            if not os.path.exists(deeper_path):
-                os.mkdir(deeper_path)
-                print(f'created {deeper_path}')
+        if not os.path.exists(pathfile):
+            os.makedirs(pathfile)
 
-        # save the file
-        save_file = os.path.join(deeper_path, filename)
-        with open(save_file, "wb") as f:
-            file_.write_to(f)
-        idx += 1
+        if not os.path.exists(save_file):
+            # do not save it if the file already exists
+            with open(save_file, "wb") as f:
+                file_.write_to(f)
+            file_idx += 1
+        else:
+            print(f'Skipping existing file {save_file}')
     print(f'saved {file_idx} files to {LOCAL_PATH}')
 
 
