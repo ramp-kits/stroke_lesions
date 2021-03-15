643994423a32e1391c25875edb20790d52403191
maikia <maja_ka@hotmail.com>
2021-03-15 17:22:52 +0100

    added unet model

--- submissions/deep_learn/estimator.py
+++ submissions/deep_learn/estimator.py
@@ -3,7 +3,12 @@ import os
 from sklearn.base import BaseEstimator
 from keras import backend as K
 from keras.layers.normalization import BatchNormalization
+from keras.layers import Deconvolution3D, MaxPooling3D, UpSampling3D
+from keras_contrib.layers.normalization.instancenormalization import \
+    InstanceNormalization
+from keras.layers import Activation
 from keras.layers import Input, Conv3D
+from keras.layers.merge import concatenate
 from keras import Model
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
@@ -197,7 +202,6 @@ class KerasSegmentationClassifier(BaseEstimator):
             shuffle=True
         )
         use_multiprocessing = True
-        
         self.model.fit(
             gen_train,
             steps_per_epoch=self._get_nb_minibatches(
@@ -226,9 +230,152 @@ class KerasSegmentationClassifier(BaseEstimator):
         model = Model(inputs=inputs, outputs=output)
         model.compile(optimizer=Adam(lr=self.initial_learning_rate),
                       loss=_dice_coefficient_loss)
-                      #metrics=[_dice_coefficient])
         print(model.summary())
         return model
+
+    def unet_model_3d(
+            self, input_shape, pool_size=(2, 2, 2), n_labels=1,
+            initial_learning_rate=0.00001, deconvolution=False,
+            depth=4, n_base_filters=32,
+            include_label_wise_dice_coefficients=False,
+            batch_normalization=False, activation_name="sigmoid"):
+        """
+        Builds the 3D UNet Keras model.f
+        :param metrics: List metrics to be calculated during model training
+            (default is dice coefficient).
+        # Todo: remove the following:
+        :param include_label_wise_dice_coefficients: If True and n_labels is
+            greater than 1, model will report the dice coefficient for each
+            label as metric.
+        :param n_base_filters: The number of filters that the first layer
+            in the convolution network will have. Following layers will contain
+            a multiple of this number. Lowering this number will likely reduce
+            the amount of memory required to train the model.
+        :param depth: indicates the depth of the U-shape for the model.
+            The greater the depth, the more max pooling layers will be added
+            to the model. Lowering the depth may reduce the amount of memory
+            required for training.
+        :param input_shape: Shape of the input data
+            (n_chanels, x_size, y_size, z_size). The x, y, and z sizes
+            must be divisible by the pool size to the power of the depth
+            of the UNet, that is pool_size^depth.
+        :param pool_size: Pool size for the max pooling operations.
+        :param n_labels: Number of binary labels that the model is learning.
+        :param initial_learning_rate: Initial learning rate for the model. This
+            will be decayed during training.
+        :param deconvolution: If set to True, will use transpose
+            convolution(deconvolution) instead of up-sampling. This increases
+            the amount memory required during training.
+        :return: Untrained 3D UNet Model
+        """
+        inputs = Input(input_shape)
+        current_layer = inputs
+        levels = list()
+
+        # add levels with max pooling
+        for layer_depth in range(depth):
+            layer1 = self._create_convolution_block(
+                input_layer=current_layer,
+                n_filters=n_base_filters*(2**layer_depth),
+                batch_normalization=batch_normalization
+                )
+            layer2 = self._create_convolution_block(
+                input_layer=layer1,
+                n_filters=n_base_filters*(2**layer_depth)*2,
+                batch_normalization=batch_normalization
+                )
+            if layer_depth < depth - 1:
+                current_layer = MaxPooling3D(pool_size=pool_size)(layer2)
+                levels.append([layer1, layer2, current_layer])
+            else:
+                current_layer = layer2
+                levels.append([layer1, layer2])
+
+        # add levels with up-convolution or up-sampling
+        for layer_depth in range(depth-2, -1, -1):
+            up_convolution = self._get_up_convolution(
+                pool_size=pool_size,
+                deconvolution=deconvolution,
+                n_filters=current_layer._keras_shape[1]
+                )(current_layer)
+            concat = concatenate(
+                [up_convolution, levels[layer_depth][1]],
+                axis=1
+                )
+            current_layer = self._create_convolution_block(
+                n_filters=levels[layer_depth][1]._keras_shape[1],
+                input_layer=concat,
+                batch_normalization=batch_normalization
+                )
+            current_layer = self._create_convolution_block(
+                n_filters=levels[layer_depth][1]._keras_shape[1],
+                input_layer=current_layer,
+                batch_normalization=batch_normalization
+                )
+
+        final_convolution = Conv3D(n_labels, (1, 1, 1))(current_layer)
+        act = Activation(activation_name)(final_convolution)
+        model = Model(inputs=inputs, outputs=act)
+
+        return model
+
+    def _create_convolution_block(
+            self, input_layer, n_filters, batch_normalization=False,
+            kernel=(3, 3, 3), activation=None,
+            padding='same', strides=(1, 1, 1),
+            instance_normalization=False):
+        """
+        :param strides:
+        :param input_layer:
+        :param n_filters:
+        :param batch_normalization:
+        :param kernel:
+        :param activation: Keras activation layer to use. (default is 'relu')
+        :param padding:
+        :return:
+        """
+        layer = Conv3D(
+            n_filters, kernel, padding=padding, strides=strides
+            )(input_layer)
+        if batch_normalization:
+            layer = BatchNormalization(axis=1)(layer)
+        elif instance_normalization:
+            layer = InstanceNormalization(axis=1)(layer)
+        if activation is None:
+            return Activation('relu')(layer)
+        else:
+            return activation()(layer)
+
+    def _compute_level_output_shape(
+            self, n_filters, depth, pool_size, image_shape):
+        """
+        Each level has a particular output shape based on the number of filters
+        used in that level and the depth or number
+        of max pooling operations that have been done on the data at that
+        point.
+        :param image_shape: shape of the 3d image.
+        :param pool_size: the pool_size parameter used in the max pooling
+            operation.
+        :param n_filters: Number of filters used by the last node in a given
+        level.
+        :param depth: The number of levels down in the U-shaped model a given
+            node is.
+        :return: 5D vector of the shape of the output node
+        """
+        output_image_shape = np.asarray(
+            np.divide(image_shape,
+                      np.power(pool_size, depth)
+                      ), dtype=np.int32).tolist()
+        return tuple([None, n_filters] + output_image_shape)
+
+    def _get_up_convolution(self, n_filters, pool_size, kernel_size=(2, 2, 2),
+                            strides=(2, 2, 2), deconvolution=False):
+        if deconvolution:
+            kernel_size = strides = pool_size
+            return Deconvolution3D(filters=n_filters, kernel_size=kernel_size,
+                                   strides=strides)
+        else:
+            return UpSampling3D(size=pool_size)
 
     def predict(self, X):
         img_loader = ImageLoader(X)
