ee6ad0cf8edcafbff815d2c34e9e92d93518e3f0
maikia <maja_ka@hotmail.com>
2021-03-31 16:54:00 +0200

    typo

--- problem.py
+++ problem.py
@@ -63,7 +63,7 @@ class DiceCoeff(BaseScoreType):
             else:
                 valid_idx = valid_indexes[idx]
             y_true = load_img_data(y_true_mask[idx])[valid_idx].astype('int8')
-            y_pred = y_pred_mask[idx][valid_idx].astype('int8')()
+            y_pred = y_pred_mask[idx][valid_idx].astype('int8')
 
             self.check_y_pred_dimensions(y_true, y_pred)
             check_mask(y_true)
