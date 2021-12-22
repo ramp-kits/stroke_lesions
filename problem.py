053b5451493711ec2a4f8673c1c288523e1795ed
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2021-12-22 05:19:53 -0800

    formatting

--- problem.py
+++ problem.py
@@ -126,6 +126,7 @@ def get_test_data(path: str):
                                       batch_size=stroke_config.testing['batch_size'])
 
     if(stroke_config.is_quick_test):
-        return bids_loader_test.data_list[:stroke_config.num_subjects_quick_test], bids_loader_test.target_list[:stroke_config.num_subjects_quick_test]
+        return bids_loader_test.data_list[:stroke_config.num_subjects_quick_test], \
+               bids_loader_test.target_list[:stroke_config.num_subjects_quick_test]
     else:
         return bids_loader_test.data_list, bids_loader_test.target_list
