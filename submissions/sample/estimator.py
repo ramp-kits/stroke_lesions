b4fe5366a3ce734905486b4b2bf5a990e409e39d
Francois Caud <frcaud@gmail.com>
2024-01-18 14:51:52 +0100

    updt submission files + setup

--- submissions/sample/estimator.py
+++ submissions/sample/estimator.py
@@ -3,20 +3,18 @@ from sklearn.base import BaseEstimator
 
 
 class BIDSEstimator(BaseEstimator):
-    '''
+    """
     Estimator sample for RAMP stroke lesion segmentation.
-    '''
+    """
 
     def __init__(self):
-        '''
+        """
         Initialize estimator values (e.g. starting learning rate) here.
-        '''
+        """
         return
 
-    def fit(self,
-            X: np.array,
-            y: np.array):
-        '''
+    def fit(self, X: np.array, y: np.array):
+        """
         Fit the estimator using the input data (X) and target (y). Assumes that all data is present. Optional.
         This estimator in particular does nothing.
         Parameters
@@ -29,11 +27,11 @@ class BIDSEstimator(BaseEstimator):
         Returns
         -------
         None
-        '''
+        """
         return
 
     def fit_partial(self, X, y):
-        '''
+        """
         Fit the estimator using the input data (X) and target (y). Assumes that the inputs represent only a fraction
         of the data and that it will be called multiple times while using the dataset. I.e., learning rates and adaptive
         parameters should not be entirely recalculated with each call to this method. Required.
@@ -48,14 +46,14 @@ class BIDSEstimator(BaseEstimator):
         Returns
         -------
         None
-        '''
+        """
 
         # Apply pre-processing to X
         # Feed to estimator
         return
 
     def predict_proba(self, X):
-        '''
+        """
         Applies the data to the estimator to produce a prediction. The output can be continuous to represent the
         relative confidence the estimator has in the prediction. Optional.
         Typically, correct but uncertain predictions are rewarded less. Similarly, incorrect but uncertain predictions
@@ -70,25 +68,25 @@ class BIDSEstimator(BaseEstimator):
         -------
         np.array
             Prediction made by the estimator.
-        '''
+        """
         y = np.ones(X.shape, dtype=bool)
         return y
 
     def predict(self, X):
-        '''
-            Applies the data to the estimator to produce a prediction. The output type is expected to match the problem.
-            I.e., classification problems should have categorical predictions. Required.
-            This estimator always returns 1.
-            Parameters
-            ----------
-            X : np.array
-                Data of the form (n_samples, n_channels, *image.shape)
+        """
+        Applies the data to the estimator to produce a prediction. The output type is expected to match the problem.
+        I.e., classification problems should have categorical predictions. Required.
+        This estimator always returns 1.
+        Parameters
+        ----------
+        X : np.array
+            Data of the form (n_samples, n_channels, *image.shape)
 
-            Returns
-            -------
-            np.array
-                Prediction made by the estimator.
-            '''
+        Returns
+        -------
+        np.array
+            Prediction made by the estimator.
+        """
         y = np.ones(X.shape, dtype=bool)
         return y
 
